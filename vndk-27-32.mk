537a5a47fd71d3c8ea8ff5c3def31390699e5225
sooti <sooti85@gmail.com>
2018-08-13 08:16:23 +0000

    vndk: add blobs for android pie

---
+++ vndk-27-32.mk
@@ -0,0 +1,16 @@
+PRODUCT_COPY_FILES += \
+	vendor/vndk/vndk-27-arm32/libandroidfw.so:system/lib/vndk-27/libandroidfw.so \
+	vendor/vndk/vndk-27-arm32/libandroid_runtime.so:system/lib/vndk-27/libandroid_runtime.so \
+	vendor/vndk/vndk-27-arm32/libandroid.so:system/lib/vndk-27/libandroid.so \
+	vendor/vndk/vndk-27-arm32/libeffectsconfig.so:system/lib/vndk-27/libeffectsconfig.so \
+	vendor/vndk/vndk-27-arm32/libinput.so:system/lib/vndk-27/libinput.so \
+	vendor/vndk/vndk-27-arm32/liblogwrap.so:system/lib/vndk-27/liblogwrap.so \
+	vendor/vndk/vndk-27-arm32/libmedia.so:system/lib/vndk-27/libmedia.so \
+	vendor/vndk/vndk-27-arm32/libnetd_client.so:system/lib/vndk-27/libnetd_client.so \
+	vendor/vndk/vndk-27-arm32/libsensorservice.so:system/lib/vndk-27/libsensorservice.so \
+	vendor/vndk/vndk-27-arm32/libsensor.so:system/lib/vndk-27/libsensor.so \
+	vendor/vndk/vndk-27-arm32/libskia.so:system/lib/vndk-27/libskia.so \
+	vendor/vndk/vndk-27-arm32/libsonivox.so:system/lib/vndk-27/libsonivox.so \
+	vendor/vndk/vndk-27-arm32/libspl.so:system/lib/vndk-27/libspl.so \
+	vendor/vndk/vndk-27-arm32/libstdc++.so:system/lib/vndk-27/libstdc++.so \
+
---
+++ vndk-27-64.mk
@@ -0,0 +1,19 @@
+PRODUCT_COPY_FILES += \
+	vendor/vndk/vndk-27-arm64/libandroidfw.so:system/lib64/vndk-27/libandroidfw.so \
+	vendor/vndk/vndk-27-arm64/libandroid_net.so:system/lib64/vndk-27/libandroid_net.so \
+	vendor/vndk/vndk-27-arm64/libandroid_runtime.so:system/lib64/vndk-27/libandroid_runtime.so \
+	vendor/vndk/vndk-27-arm64/libandroid.so:system/lib64/vndk-27/libandroid.so \
+	vendor/vndk/vndk-27-arm64/libeffectsconfig.so:system/lib64/vndk-27/libeffectsconfig.so \
+	vendor/vndk/vndk-27-arm64/libinput.so:system/lib64/vndk-27/libinput.so \
+	vendor/vndk/vndk-27-arm64/liblogwrap.so:system/lib64/vndk-27/liblogwrap.so \
+	vendor/vndk/vndk-27-arm64/libmedia.so:system/lib64/vndk-27/libmedia.so \
+	vendor/vndk/vndk-27-arm64/libnetdaidl.so:system/lib64/vndk-27/libnetdaidl.so \
+	vendor/vndk/vndk-27-arm64/libnetd_client.so:system/lib64/vndk-27/libnetd_client.so \
+	vendor/vndk/vndk-27-arm64/libnetdutils.so:system/lib64/vndk-27/libnetdutils.so \
+	vendor/vndk/vndk-27-arm64/libsensorservice.so:system/lib64/vndk-27/libsensorservice.so \
+	vendor/vndk/vndk-27-arm64/libsensor.so:system/lib64/vndk-27/libsensor.so \
+	vendor/vndk/vndk-27-arm64/libskia.so:system/lib64/vndk-27/libskia.so \
+	vendor/vndk/vndk-27-arm64/libsonivox.so:system/lib64/vndk-27/libsonivox.so \
+	vendor/vndk/vndk-27-arm64/libspl.so:system/lib64/vndk-27/libspl.so \
+	vendor/vndk/vndk-27-arm64/libstdc++.so:system/lib64/vndk-27/libstdc++.so \
+
