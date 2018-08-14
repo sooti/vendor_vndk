86985f742132947aeab547c106fc10838d28cde4
sooti <sooti85@gmail.com>
2018-08-14 19:50:01 +0000

    vndk: update generate.sh for vndk 27

--- generate.sh
+++ generate.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo "PRODUCT_COPY_FILES += \\" > vndk32.mk
-echo "PRODUCT_COPY_FILES += \\" > vndk64.mk
-for lib in $(ls vndk-26-arm32);do
-	echo -e "\tvendor/vndk/vndk-26-arm32/$lib:system/lib/vndk-26/$lib \\" >> vndk32.mk
+echo "PRODUCT_COPY_FILES += \\" > vndk-27-32.mk
+echo "PRODUCT_COPY_FILES += \\" > vndk-27-64.mk
+for lib in $(ls vndk-27-arm32);do
+	echo -e "\tvendor/vndk/vndk-27-arm32/$lib:system/lib/vndk-27/$lib \\" >> vndk-27-32.mk
 done
-for lib in $(ls vndk-sp-26-arm32);do
-	echo -e "\tvendor/vndk/vndk-sp-26-arm32/$lib:system/lib/vndk-sp-26/$lib \\" >> vndk32.mk
+#for lib in $(ls vndk-sp-27-arm32);do
+#	echo -e "\tvendor/vndk/vndk-sp-27-arm32/$lib:system/lib/vndk-sp-27/$lib \\" >> vndk32.mk
+#done
+for lib in $(ls vndk-27-arm64);do
+	echo -e "\tvendor/vndk/vndk-27-arm64/$lib:system/lib64/vndk-27/$lib \\" >> vndk-27-64.mk
 done
-for lib in $(ls vndk-26-arm64);do
-	echo -e "\tvendor/vndk/vndk-26-arm64/$lib:system/lib64/vndk-26/$lib \\" >> vndk64.mk
-done
-for lib in $(ls vndk-sp-26-arm64);do
-	echo -e "\tvendor/vndk/vndk-sp-26-arm64/$lib:system/lib64/vndk-sp-26/$lib \\" >> vndk64.mk
-done
-echo >> vndk32.mk
-echo >> vndk64.mk
+#for lib in $(ls vndk-sp-27-arm64);do
+#	echo -e "\tvendor/vndk/vndk-sp-27-arm64/$lib:system/lib64/vndk-sp-27/$lib \\" >> vndk64.mk
+#done
+echo >> vndk-27-32.mk
+echo >> vndk-27-64.mk
